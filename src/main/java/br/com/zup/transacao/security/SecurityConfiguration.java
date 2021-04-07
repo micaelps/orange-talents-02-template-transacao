b8d1cd5772e4a7c62871f29680d42acb5e04d331
micaelps <3.1416micael@gmail.com>
2021-04-07 06:40:50 -0300

    feat: autenticacao/autorizacao

---
+++ src/main/java/br/com/zup/transacao/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.zup.transacao.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+@Configuration
+@Profile("!dev")
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);;
+    }
+}
